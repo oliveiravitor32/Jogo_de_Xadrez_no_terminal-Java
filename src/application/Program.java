2d4c3b1c6b7cc399e7446239299885a8b831edcf
oliveiravitor32 <oliveiravitor327@gmail.com>
2023-10-30 15:16:50 -0300

    Created clear screen.

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,9 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
@@ -14,15 +16,24 @@ public class Program {
 		ChessMatch chessMatch = new ChessMatch();
 		
 		while (true) {
-			System.out.println();
-			UI.printBoard(chessMatch.getPieces());
-			System.out.print("\nSource: ");
-			ChessPosition source = UI.readChessPosition(sc);
-			
-			System.out.print("Target: ");
-			ChessPosition target = UI.readChessPosition(sc);
-			
-			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			try {
+				UI.clearScreen();
+				System.out.println();
+				UI.printBoard(chessMatch.getPieces());
+				System.out.print("\nSource: ");
+				ChessPosition source = UI.readChessPosition(sc);
+				
+				System.out.print("Target: ");
+				ChessPosition target = UI.readChessPosition(sc);
+				
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			} catch (ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			} catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
 		}
 	}
 }
