662153cd74bfbf904b81f72fecaa0f90828ccbb5
oliveiravitor32 <oliveiravitor327@gmail.com>
2023-10-30 14:52:47 -0300

    Created the movement of the pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,28 @@
 package application;
 
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
-		UI.printBoard(chessMatch.getPieces());
+		
+		while (true) {
+			System.out.println();
+			UI.printBoard(chessMatch.getPieces());
+			System.out.print("\nSource: ");
+			ChessPosition source = UI.readChessPosition(sc);
+			
+			System.out.print("Target: ");
+			ChessPosition target = UI.readChessPosition(sc);
+			
+			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+		}
 	}
 }
