ba81445993536306d566e24761f1c9c104eb1d0b
oliveiravitor32 <oliveiravitor327@gmail.com>
2023-11-01 16:14:55 -0300

    Created Queen

--- src/chess/pieces/Bishop.java
+++ src/chess/pieces/Bishop.java
@@ -20,7 +20,7 @@ public class Bishop extends ChessPiece {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
 		
 		Position p = new Position(0, 0);
-
+		
 		// nw
 		p.setValues(position.getRow() - 1, position.getColumn() -1);
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
